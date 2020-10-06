0bf42fa0a493bedcd04cc6bf829aabd213536e4c
Fransosiche <francois.biron@viacesi.fr>
2020-10-06 20:16:37 +0200

    New users in db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -30,7 +30,39 @@ class CreateUsersTable extends Migration
 
         $New_User->name="François Biron";
         $New_User->email="francois.biron@viacesi.fr";
-        $New_User->password=Hash::make("test123");
+        $New_User->password=Hash::make("qovjei2qv");
+        $New_User->is_admin="1";
+        $New_User->save();
+
+        $New_User = new User();
+
+        $New_User->name="Valérie Sibioude";
+        $New_User->email="valeriesibioude@hotmail.fr";
+        $New_User->password=Hash::make("3ECDt7eS");
+        $New_User->is_admin="1";
+        $New_User->save();
+
+        $New_User = new User();
+
+        $New_User->name="Christian Moriceau";
+        $New_User->email="moriceauchristian@orange.fr";
+        $New_User->password=Hash::make("3ECDt7eS");
+        $New_User->is_admin="1";
+        $New_User->save();
+
+        $New_User = new User();
+
+        $New_User->name="Yoan Maillard";
+        $New_User->email="yoansuaude@gmail.com";
+        $New_User->password=Hash::make("2vePd7y7");
+        $New_User->is_admin="1";
+        $New_User->save();
+
+        $New_User = new User();
+
+        $New_User->name="Coline Belkheir";
+        $New_User->email="colineblkhr@hotmail.com";
+        $New_User->password=Hash::make("Eyz8We3b");
         $New_User->is_admin="1";
         $New_User->save();
     }
